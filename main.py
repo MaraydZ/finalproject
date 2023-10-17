d9d20805221b66d005ecf19b594a411156db3662
marayd <93208430+MaraydZ@users.noreply.github.com>
2023-10-17 20:59:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import html
 from bs4 import BeautifulSoup
 import sqlite3
 import os
+import time
 
 class AppConfig:
     def config(self):
         self.address = "localhost"
         self.port = "8080"
-        self.start_web_at_run = False
+        self.start_web_at_run = True
 
 class Hello:
     def __init__(self) -> None:
@@ -35,33 +36,33 @@ class AppRun:
         else:
             pass
 
-class Database:
-    def __init__(self, db_name):
-        self.conn = sqlite3.connect(db_name)
-        self.cursor = self.conn.cursor()
-        self.create_table()
+# class Database:
+#     def __init__(self, db_name):
+#         self.conn = sqlite3.connect(db_name)
+#         self.cursor = self.conn.cursor()
+#         self.create_table()
 
-    def create_table(self):
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS websites (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                url TEXT)''')
-        self.conn.commit()
+#     def create_table(self):
+#         self.cursor.execute('''CREATE TABLE IF NOT EXISTS websites (
+#                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                                 url TEXT)''')
+#         self.conn.commit()
 
-    def add_website(self, url):
-        self.cursor.execute("INSERT INTO websites (url) VALUES (?)", (url,))
-        self.conn.commit()
+#     def add_website(self, url):
+#         self.cursor.execute("INSERT INTO websites (url) VALUES (?)", (url,))
+#         self.conn.commit()
 
-    def clear_database(self):
-        self.cursor.execute("DELETE FROM websites")
-        self.conn.commit()
+#     def clear_database(self):
+#         self.cursor.execute("DELETE FROM websites")
+#         self.conn.commit()
 
-    def get_all_websites(self):
-        self.cursor.execute("SELECT * FROM websites")
-        return self.cursor.fetchall()
+#     def get_all_websites(self):
+#         self.cursor.execute("SELECT * FROM websites")
+#         return self.cursor.fetchall()
 
-    def get_search_queries(self):
-        self.cursor.execute("SELECT DISTINCT url FROM websites")
-        return [row[0] for row in self.cursor.fetchall()]
+#     def get_search_queries(self):
+#         self.cursor.execute("SELECT DISTINCT url FROM websites")
+#         return [row[0] for row in self.cursor.fetchall()]
 
 class WebsiteParser:
     def __init__(self):
@@ -101,7 +102,8 @@ class GoogleSearch:
                     url_parts = decoded_url.split("\\")
                     cleaned_url = url_parts[0]
                     cleaned_url = cleaned_url.replace('"', '')
-                    if not cleaned_url.startswith(('https://www.w3.org', 'https://www.google.com', "https://policies.google.com", "https://accounts.google.com", "https://support.google.com")):
+                    if not cleaned_url.startswith(('https://www.w3.org', 'https://www.google.com', "https://policies.google.com", "https://accounts.google.com", "https://support.google.com")) \
+                                and 'google' not in cleaned_url and not 'yandex' in cleaned_url and not 'tiktok' in cleaned_url:
                         urls.append(cleaned_url)
             return urls
         return []
@@ -198,54 +200,51 @@ app = Bottle()
 def index():
     return template('index')
 
-@app.route('/add_website', method='POST')
-def add_website():
-    url = request.forms.get('url')
-    if url:
-        if url not in unique_websites:
-            unique_websites.add(url)
-            database.add_website(url)
-        return redirect('/')
-    else:
-        return "Введите URL сайта."
+# @app.route('/add_website', method='POST')
+# def add_website():
+#     url = request.forms.get('url')
+#     if url:
+#         if url not in unique_websites:
+#             unique_websites.add(url)
+#             database.add_website(url)
+#         return redirect('/')
+#     else:
+#         return "Введите URL сайта."
 
-@app.route('/clear_database', method='POST')
-def clear_database():
-    database.clear_database()
-    unique_websites.clear()
-    return redirect('/')
+# @app.route('/clear_database', method='POST')
+# def clear_database():
+#     database.clear_database()
+#     unique_websites.clear()
+#     return redirect('/')
 
-@app.route('/view_websites')
-def view_websites():
-    websites = database.get_all_websites()
-    if not websites:
-        return '<!DOCTYPE html><html><head></head><body><script>alert("База данных пуста!"); location.href = "/"</script></body></html>'
-    else:
-        return template('view_websites', websites=websites)
+# @app.route('/view_websites')
+# def view_websites():
+#     websites = database.get_all_websites()
+#     if not websites:
+#         return '<!DOCTYPE html><html><head></head><body><script>alert("База данных пуста!"); location.href = "/"</script></body></html>'
+#     else:
+#         return template('view_websites', websites=websites)
 
 @app.route('/search', method='POST')
 def search():
     global unique_websites
-    database_queries = database.get_search_queries()
-    if not database_queries:
-        return '<!DOCTYPE html><html><head></head><body><script>alert("База данных запросов пуста. Добавьте запросы для поиска"); location.href = "/"</script></body></html>'
+    url = request.forms.get('url')  
+    query_keywords = url.split()  
     results = []
 
     urls = []
     scores = []
 
-    for query in database_queries:
-        query_keywords = [keyword.strip() for keyword in query.split(' ')]
-        google_results = google_search.search(query_keywords)
+    for keyword in query_keywords: 
+        google_results = google_search.search([keyword])
 
         if google_results:
             for result_url in google_results:
                 if result_url not in unique_websites:
-                    
                     unique_websites.add(result_url)
-                    website_keywords = website_parser.search_keywords(result_url, query_keywords)
-                    google_search_instance = GoogleSearch()  
-                    score = google_search_instance.calculate_score(url=result_url, keywords=query_keywords)
+                    website_keywords = website_parser.search_keywords(result_url, [keyword])
+                    google_search_instance = GoogleSearch()
+                    score = google_search_instance.calculate_score(url=result_url, keywords=[keyword])
                     urls.append(result_url)
                     scores.append(score)
 
@@ -254,6 +253,7 @@ def search():
     results.sort(key=lambda x: x[1], reverse=True)
 
     return template('search_results', results=results)
+
 
 # @app.route('/yasearch', method='POST')
 # def yasearch():
@@ -288,7 +288,7 @@ def search():
 if __name__ == '__main__':
     Hello = Hello()
     Hello.hello(value="hello")
-    database = Database("websites.db")
+    # database = Database("websites.db")
     website_parser = WebsiteParser()
     google_search = GoogleSearch()
     # yandex_search = YandexSearch()
@@ -298,3 +298,4 @@ if __name__ == '__main__':
     app_config = AppConfig()
     app_config.config()
     run(app, host=app_config.address, port=app_config.port)
+
